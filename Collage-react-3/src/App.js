3639796b0a4c6ea1ef5678a62e5e9acc6d4bf997
Ezzo <90714523+TENORvm@users.noreply.github.com>
2023-09-09 19:31:35 +0300

    Add files via upload

---
+++ Collage-react-3/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Review from './Review';
+
+
+
+function App() {
+    return ( <
+        main >
+        <
+        section className = 'container' >
+        <
+        div className = 'title' >
+        <
+        h2 > Out reviews < /h2> <
+        /div> <
+        Review / >
+        <
+        /section> <
+        /main>
+    );
+}
+
+
+export default App;
