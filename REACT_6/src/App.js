a40f4fced209bbcc7e6e5130dda0cd8ec7131c1b
Ezzo <90714523+TENORvm@users.noreply.github.com>
2023-10-05 14:13:59 +0300

    Add files via upload

---
+++ REACT_6/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+
+function App() {
+    const [people, setPeople] = useState(data);
+    const [index, setIndex] = useState(0);
+
+   
+    const nextSlide = () => {
+        setIndex((prevIndex) => (prevIndex + 1) % people.length);
+    };
+
+    
+    const prevSlide = () => {
+        setIndex((prevIndex) => (prevIndex - 1 + people.length) % people.length);
+    };
+
+    
+    useEffect(() => {
+        const interval = setInterval(nextSlide, 2000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <section className='section'>
+            <div className='title'>
+                <h2><span>/</span> reviews</h2>
+            </div>
+            <div className='section-center'>
+                {people.map((person, personIndex) => {
+                    const { id, image, name, title, quote } = person;
+                    let position = "nextSlide";
+                    if (personIndex === index) {
+                        position = "activeSlide";
+                    }
+                    if (personIndex === index - 1 || (personIndex === people.length - 1 && index === 0)) {
+                        position = "lastSlide";
+                    }
+                    return (
+                        <article key={id} className={position}>
+                            <img src={image} alt={name} className='person-img' />
+                            <h4>{name}</h4>
+                            <p className='title'>{title}</p>
+                            <p className='text'>{quote}</p>
+                            <FaQuoteRight className='icon' />
+                        </article>
+                    );
+                })}
+                <button className='prev' onClick={prevSlide}>
+                    <FiChevronLeft />
+                </button>
+                <button className='next' onClick={nextSlide}>
+                    <FiChevronRight />
+                </button>
+            </div>
+        </section>
+    );
+}
+
+export default App;
