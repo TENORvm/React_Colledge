058c87bcc7eed224066bbc07beb939016da6156f
Ezzo <90714523+TENORvm@users.noreply.github.com>
2023-12-09 14:26:30 +0200

    Add files via upload

---
+++ REACT_12/src/components/SearchForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useGlobalContext } from '../context';
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  }
+
+  const handleSubmit = (event) => {
+    const searchTerm = searchValue.current.value.trim(); 
+
+    if (searchTerm) {
+ 
+      searchCocktail();
+    }
+  }
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  return (
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default SearchForm;
---
+++ REACT_12/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at placerat nunc. Nullam gravida cursus felis id ultricies. Vivamus laoreet quam ut turpis ultricies, a vestibulum quam varius. Vivamus a sem in turpis vestibulum placerat vitae non nulla. Morbi nec malesuada velit. Nullam vel vehicula orci, vel feugiat ex. Sed et eros vel dolor semper facilisis ac et eros. Curabitur tincidunt, nunc sit amet tristique elementum, sapien mauris luctus ex, non luctus lectus purus malesuada tellus. Sed metus ante, elementum non est eget, commodo volutpat orci. Curabitur interdum sodales quam, in ullamcorper augue porttitor in. Donec interdum justo sagittis hendrerit malesuada. Praesent vel nibh vel tortor congue posuere a a augue. Phasellus fringilla nec odio vitae lobortis. In a ornare turpis. Nullam turpis metus, sollicitudin quis imperdiet vel, tincidunt non lorem. Cras at consequat justo, nec pulvinar lectus.</p>
+    </section>
+  )
+}
+
+export default About
